42336f8b02dc9a554e8ea06e76f4ca2c0d64f46c
joshua762002 <joshuaeviota60@gmail.com>
2024-11-22 21:40:14 +0800

    ADD

---
+++ script.js
@@ -0,0 +1,11 @@
+function openModal(title, description) {
+    document.getElementById('modal-title').textContent = title;
+    document.getElementById('modal-description').textContent = description;
+    document.getElementById('modal').style.display = 'flex';
+  }
+  
+  function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+  }
+  
+  
